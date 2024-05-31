d9de106f32d4e1b0f63dddd360f02c6238e96053
Ajeem Ahmad <ahmedazim7804@gmail.com>
2024-05-31 18:37:30 +0530

    some fixes

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,14 @@ import App from "./pages/app";
 import "./main.css";
 import { AuthenticationPage } from "./pages/authentication_page";
 import { AuthProvider } from "./state/providers/authProvider";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
         <AuthProvider>
             <BrowserRouter>
                 <Routes>
+                    <Route path="/" element={<Navigate to="/auth" />} />
                     <Route path="/auth/*" element={<AuthenticationPage />} />
                     <Route path="/app" element={<App />} />
                 </Routes>
--- src/pages/authentication_page.jsx
+++ src/pages/authentication_page.jsx
@@ -1,6 +1,23 @@
+import { useEffect } from "react";
 import { AuthBox } from "../components/authBox/auth_box";
+import { supabase } from "../services/supabase.jsx";
+import { useNavigate } from "react-router-dom";
 
 export function AuthenticationPage() {
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        async function checkUser() {
+            const user = await supabase.auth.getUser();
+
+            if (user.data != null && user.data.user.id) {
+                navigate("/app");
+            }
+        }
+
+        checkUser();
+    }, [navigate]);
+
     return (
         <div>
             <AuthBox></AuthBox>
