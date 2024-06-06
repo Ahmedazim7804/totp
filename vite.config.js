48a4563e265b5b0b853bde24597c5ef15f4c51cc
Ajeem Ahmad <ahmedazim7804@gmail.com>
2024-06-06 19:08:58 +0530

    added envs for vercel

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import eslintPlugin from "vite-plugin-eslint";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    define: {
+        __APP_ENV__: process.env.VITE_VERCEL_ENV,
+    },
     plugins: [react()],
 });
